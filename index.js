ac335ee0ecd5633d7e60e6b29a6470f13f8fe842
Bhargavbhimani229 <bhargavbhimani229@gmail.com>
2025-02-18 10:09:19 +0530

    done

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ const db = require("./configs/database");
 const adminModel = require("./models/adminschema");
 const Book = require("./models/bookSchema");
 const app = express();
-const port = 8055;
+const port = 8059;
 let users = [];
 let loginUser = {
   username: "bhargav@123",
   password: "bhargav@123",
 };
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs"); 
+app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true })); 
 
-// Routes
+// ---------------------- Home Route ----------------------
 app.get("/", (req, res) => {
-  Book.find()
+  Book.find({})
     .then((allBooks) => {
-      res.render("index", { books: allBooks });
+      const bestSeller = allBooks.filter(book => book.bestseller === true);
+      console.log("Bestseller book", allBooks);
+      
+      const popular = allBooks.filter(book => book.bestseller === false);
+      
+      res.render("index", { bestSeller, popular });
     })
     .catch((err) => {
       console.error(err);
@@ -25,65 +31,112 @@ app.get("/", (req, res) => {
     });
 });
 
+// ---------------------- Login Route ----------------------
 app.get("/login", (req, res) => {
-
-  res.render("login",{error:null});
+  res.render("login", { error: null }); 
 });
+
+// ---------------------- Book Form Route ----------------------
 app.get("/bookform", (req, res) => {
-  return res.render("bookform");
-});
-app.get("/logout", (req, res) => {
-  res.redirect("/login");
+  return res.render("bookform"); 
 });
 
-app.get("/bookList", (req, res) => {
-  Book.find({})
-    .then((books) => {
-      res.render("bookList", { books });
+// ---------------------- Logout Route ----------------------
+app.get("/logout", (req, res) => {
+  res.redirect("/login"); 
+});
+
+// ---------------------- Edit Book (GET) ----------------------
+app.get('/book/edit/:id', (req, res) => {
+  const { id } = req.params;
+  Book.findById(id)
+    .then((book) => {
+      return res.render('editbook', { book }); 
     })
     .catch((err) => {
       console.log(err.message);
-      res.render("bookList", { books: [] });
+      return res.render('editbook', { book: [] }); 
     });
 });
 
-app.get('/book/delete/:id',(req,res)=>{
-  const {id} = req.params;
-
-  Book.findByIdAndDelete(id).then(()=>{
-    return res.redirect(req.get("Referrer") || '/bookList');
-  }).catch((err)=>{
-    console.log(err.message);
-    return res.redirect(req.get("Referrer") || '/bookList');
+// ---------------------- Edit Book (POST) ----------------------
+app.post('/book/edit', (req, res) => {
+  const { id, bookName, author, price, description, imageUrl, bestseller } = req.body;
+  Book.findByIdAndUpdate(id, {
+    bookName,
+    author,
+    price,
+    description,
+    imageUrl,
+    bestseller: bestseller === 'on' 
   })
-})
+  .then(() => {
+    console.log("Book Updated...");
+    return res.redirect('/bookList'); 
+  })
+  .catch((err) => {
+    console.log(err.message);
+    return res.redirect('/bookList'); 
+  });
+});
 
+// ---------------------- Book List Route ----------------------
+app.get("/bookList", (req, res) => {
+  Book.find({})
+    .then((books) => {
+      res.render("bookList", { books }); 
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.render("bookList", { books: [] }); 
+    });
+});
 
+// ---------------------- Delete Book Route ----------------------
+app.get('/book/delete/:id', (req, res) => {
+  const { id } = req.params;
+
+  Book.findByIdAndDelete(id)
+    .then(() => {
+      return res.redirect(req.get("Referrer") || '/bookList'); 
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.redirect(req.get("Referrer") || '/bookList'); 
+    });
+});
+
+// ---------------------- Login Authentication (POST) ----------------------
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
   if (email === "bhargav@123" && password === "bhargav@123") {
-    return res.render("bookform");
+    return res.render("bookform"); 
   } else {
-    res.render("login", { error: "Invalid username or password" });
+    res.render("login", { error: "Invalid username or password" }); 
   }
 });
 
+// ---------------------- Add Book Route ----------------------
 app.post("/addbook", (req, res) => {
+  console.log(req.body);
+  
+  req.body.bestseller = req.body.bestseller === 'on'; 
+
   Book.create(req.body)
-    .then(() => {
-      console.log("Book Created");
+    .then((data) => {
+      console.log(req.body);
       res.redirect("/bookList"); 
     })
     .catch((err) => {
       console.log(err.message);
-      res.render("bookform");
+      res.render("bookform"); 
     });
 });
 
-
+// ---------------------- Start Server ----------------------
 app.listen(port, (err) => {
   if (!err) {
     console.log("Server started on this Port");
-    console.log("http://localhost:" + port);
+    console.log("http://localhost:" + port); 
   }
 });
--- models/bookSchema.js
+++ models/bookSchema.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema({
+const bookSchema = new mongoose.Schema(
+  {
     bookName: { type: String, required: true },
     author: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    imageUrl: { type: String, required: true }
-}, { timestamps: true });
+    imageUrl: { type: String, required: true },
+    bestseller: { type: Boolean },
+  },
+  { timestamps: true }
+);
 
 const Book = mongoose.model("Book", bookSchema);
 module.exports = Book;
